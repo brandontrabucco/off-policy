89687c9c552086466e9152bda7c7d58e55ac4ab7
Brandon <brandon@btrabucco.com>
2020-06-28 17:58:47 -0700

    update train loading

--- offpolicy/__init__.py
+++ offpolicy/__init__.py
@@ -2,3 +2,70 @@ import tensorflow as tf
 
 for gpu in tf.config.experimental.list_physical_devices('GPU'):
     tf.config.experimental.set_memory_growth(gpu, True)
+
+
+def train(logging_dir,
+          training_env,
+          eval_env,
+          alg):
+    """Train a policy using an off policy reinforcement learning
+    algorithm such as SAC or TD3
+
+    Args:
+
+    logging_dir: str
+        the disk path to where metrics and weights will be saved
+    training_env: gym.Env
+        an environment on which a policy shall be trained
+    eval_env: gym.Env
+        an environment on which a policy shall be evaluated
+    alg: str
+        a string identifier that indicates which algorithm to use
+    """
+
+    from offpolicy.replay_buffer import ReplayBuffer
+    from offpolicy.algorithms import get_algorithm
+    from offpolicy.logger import Logger
+    from offpolicy.env import Env
+    from offpolicy.trainer import Trainer
+    import os
+
+    n = 1000000
+    logger = Logger(logging_dir)
+    training_env = Env(training_env)
+    eval_env = Env(eval_env)
+
+    act_size = training_env.action_space.shape[0]
+    obs_size = training_env.observation_space.shape[0]
+
+    alg = get_algorithm(
+        alg,
+        training_env.action_space.low,
+        training_env.action_space.high,
+        obs_size,
+        act_size)
+
+    b = ReplayBuffer(1000000, obs_size, act_size)
+
+    trainer = Trainer(
+        training_env,
+        eval_env,
+        alg.policy,
+        b,
+        alg)
+
+    ckpt = tf.train.Checkpoint(**trainer.get_saveables())
+    path = tf.train.latest_checkpoint(logging_dir)
+    if path is not None:
+        ckpt.restore(path)
+
+    while b.step < n:
+
+        trainer.train()
+
+        if b.step % 5000 == 0:
+            for key, value in trainer.get_diagnostics().items():
+                logger.record(key, value, tf.cast(b.step, tf.int64))
+
+        if b.step % 10000 == 0 and b.step > 0:
+            ckpt.save(os.path.join(logging_dir, 'alg.ckpt'))
--- train.py
+++ train.py
@@ -1,70 +1,10 @@
-from offpolicy.replay_buffer import ReplayBuffer
-from offpolicy.algorithms import get_algorithm
-from offpolicy.logger import Logger
-from offpolicy.env import Env
-from offpolicy.trainer import Trainer
-import tensorflow as tf
-import os
+from offpolicy import train
 import gym
-
-
-def train(logging_dir,
-          training_env,
-          eval_env,
-          alg):
-    """Train a policy using an off policy reinforcement learning
-    algorithm such as SAC or TD3
-
-    Args:
-
-    logging_dir: str
-        the disk path to where metrics and weights will be saved
-    training_env: gym.Env
-        an environment on which a policy shall be trained
-    eval_env: gym.Env
-        an environment on which a policy shall be evaluated
-    alg: str
-        a string identifier that indicates which algorithm to use
-    """
-
-    logger = Logger(logging_dir)
-    training_env = Env(training_env)
-    eval_env = Env(eval_env)
-
-    act_size = training_env.action_space.shape[0]
-    obs_size = training_env.observation_space.shape[0]
-
-    alg = get_algorithm(alg,
-                        training_env.action_space.low,
-                        training_env.action_space.high,
-                        obs_size,
-                        act_size)
-
-    trainer = Trainer(
-        training_env,
-        eval_env,
-        alg.policy,
-        ReplayBuffer(1000000, obs_size, act_size),
-        alg)
-
-    ckpt = tf.train.Checkpoint(**trainer.get_saveables())
-
-    latest_ckpt = tf.train.latest_checkpoint(logging_dir)
-    if latest_ckpt is not None:
-        ckpt.restore(latest_ckpt)
-
-    for i in range(1000000):
-        trainer.train()
-        if i % 5000 == 0:
-            for key, value in trainer.get_diagnostics().items():
-                logger.record(key, value, tf.cast(i, tf.dtypes.int64))
-        if i % 10000 == 0 and i > 0:
-            ckpt.save(os.path.join(logging_dir, 'alg.ckpt'))
 
 
 if __name__ == "__main__":
 
-    train('./half_cheetah_td32',
+    train('./half_cheetah_td3',
           gym.make('HalfCheetah-v2'),
           gym.make('HalfCheetah-v2'),
           'TD3')
